12039969bf9a4410489819df0c55cda4a7cab499
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-06 23:05:52 +0300

    added program of first task

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+while (threeDigitNumber < 1000)
+{
+    int n = threeDigitNumber / 10;
+    if (n < 0) n = -n;
+    int result = n % 10;
+    Console.WriteLine($"Вторая цифра этого числа: {result}");
+    break;
+}
+
