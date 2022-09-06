c7bbdbd06b29f16789b3809b26a3b94196822ec3
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-06 18:10:04 +0300

    third task programm added

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,4 +1,4 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿// 1 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 // Console.WriteLine("Введите 2 числа для определения большего из них");
 // int firstNumber, secondNumber;
@@ -16,27 +16,44 @@
 //     Console.WriteLine($"max = {secondNumber}");
 // }
 
-// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Введите 3 числа для определения большего из них");
-int firstNumber, secondNumber, thirdNumber;
-Console.Write("Введите первое число: ");
-firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-secondNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третье число: ");
-thirdNumber = Convert.ToInt32(Console.ReadLine());
+// 2 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-int max = firstNumber;
-if (secondNumber > max)
+
+// Console.WriteLine("Введите 3 числа для определения большего из них");
+// int firstNumber, secondNumber, thirdNumber;
+// Console.Write("Введите первое число: ");
+// firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+// int max = firstNumber;
+// if (secondNumber > max)
+// {
+// Console.WriteLine($"max = {secondNumber}");
+// }
+// if (thirdNumber > max) 
+// {
+// Console.WriteLine($"max = {thirdNumber}");
+// }
+// else 
+// {
+// Console.WriteLine($"max = {firstNumber}"); 
+// }
+
+
+// 3 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+
+Console.Write("Введите число, чтобы определить четное ли оно: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
 {
-Console.WriteLine($"max = {secondNumber}");
-}
-if (thirdNumber > max) 
-{
-Console.WriteLine($"max = {thirdNumber}");
+    Console.WriteLine($"{number}: четное");
 }
 else 
 {
-Console.WriteLine($"max = {firstNumber}"); 
+    Console.WriteLine($"{number}: нечетное");
 }
