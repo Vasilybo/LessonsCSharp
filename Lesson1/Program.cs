0645f1990a8421cf55f6c2f035a50a4de48e4429
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-06 18:00:52 +0300

    second task project added

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,15 +1,42 @@
-﻿Console.WriteLine("Введите 2 числа для определения большего из них");
-int firstNumber, secondNumber;
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите 2 числа для определения большего из них");
+// int firstNumber, secondNumber;
+// Console.Write("Введите первое число: ");
+// firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// secondNumber = Convert.ToInt32(Console.ReadLine());
+ 
+// if (firstNumber > secondNumber)
+// {
+//     Console.WriteLine($"max = {firstNumber}");
+// }
+// else
+// {
+//     Console.WriteLine($"max = {secondNumber}");
+// }
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите 3 числа для определения большего из них");
+int firstNumber, secondNumber, thirdNumber;
 Console.Write("Введите первое число: ");
 firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 secondNumber = Convert.ToInt32(Console.ReadLine());
- 
-if (firstNumber > secondNumber)
+Console.Write("Введите третье число: ");
+thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = firstNumber;
+if (secondNumber > max)
 {
-    Console.WriteLine($"max = {firstNumber}");
+Console.WriteLine($"max = {secondNumber}");
 }
-else
+if (thirdNumber > max) 
 {
-    Console.WriteLine($"max = {secondNumber}");
+Console.WriteLine($"max = {thirdNumber}");
+}
+else 
+{
+Console.WriteLine($"max = {firstNumber}"); 
 }
