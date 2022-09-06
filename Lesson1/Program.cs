ce6bae476de3130ff67e25350c47b3db7200e0e5
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-06 17:46:31 +0300

    first task programm

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите 2 числа для определения большего из них");
+int firstNumber, secondNumber;
+Console.Write("Введите первое число: ");
+firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+secondNumber = Convert.ToInt32(Console.ReadLine());
+ 
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine($"max = {firstNumber}");
+}
+else
+{
+    Console.WriteLine($"max = {secondNumber}");
+}
