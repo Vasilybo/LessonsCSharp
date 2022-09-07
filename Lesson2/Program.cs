be93e4d4f55c30d73c33795b60fb1532b09c5947
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-07 14:53:35 +0300

    added program for second task

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,19 +1,33 @@
 ﻿// 1 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 
-Console.Write("Введите трёхзначное число: ");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите трёхзначное число: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 
-while (threeDigitNumber < 1000)
-{
-    int n = threeDigitNumber / 10;
-    if (n < 0) n = -n;
-    int result = n % 10;
-    Console.WriteLine($"Вторая цифра этого числа: {result}");
-    break;
-}
+// while (threeDigitNumber < 1000)
+// {
+//     int n = threeDigitNumber / 10;
+//     if (n < 0) n = -n;
+//     int result = n % 10;
+//     Console.WriteLine($"Вторая цифра этого числа: {result}");
+//     break;
+// }
 
 
 // 2 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int number = n;
+
+while (number >= 1000)
+{
+    if (n < 0) n = -n;
+    number = number / 10;
+}
+
+int result = number % 10;
+
+Console.WriteLine($"Третья цифра этого числа: {result}");
