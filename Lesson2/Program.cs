3baefd89ff638ea7f9227a9564dd3da14ab1ce5d
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-08 15:38:55 +0300

    added last program task

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,20 +1,20 @@
-﻿// 1 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿1 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 
-// Console.Write("Введите трёхзначное число: ");
-// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
 
-// while (threeDigitNumber < 1000)
-// {
-//     int n = threeDigitNumber / 10;
-//     if (n < 0) n = -n;
-//     int result = n % 10;
-//     Console.WriteLine($"Вторая цифра этого числа: {result}");
-//     break;
-// }
+while (threeDigitNumber < 1000)
+{
+    int n = threeDigitNumber / 10;
+    if (n < 0) n = -n;
+    int result = n % 10;
+    Console.WriteLine($"Вторая цифра этого числа: {result}");
+    break;
+}
 
 
-// 2 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+2 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 
 Console.Write("Введите число: ");
@@ -31,3 +31,21 @@ while (number >= 1000)
 int result = number % 10;
 
 Console.WriteLine($"Третья цифра этого числа: {result}");
+
+
+3 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("Выходной");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Это не день недели");
+  }
+  else Console.WriteLine("Это будний день");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
