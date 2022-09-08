70dee66ac8298e8b4dd6e0ce6ea0e996876c0b27
Vasilybo <swagrec.vasilybo@gmail.com>
2022-09-08 15:42:20 +0300

    rewrite

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -35,7 +35,7 @@ Console.WriteLine($"Третья цифра этого числа: {result}");
 
 3 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-Console.Write("Введи цифру, обозначающую день недели: ");
+Console.Write("Введите цифру, обозначающую день недели: ");
 int dayNumber = Convert.ToInt32(Console.ReadLine());
 
 void CheckingTheDayOfTheWeek (int dayNumber) {
